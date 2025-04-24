fa5c450c1137f05dca61fc31d03a1b2614c7a016
ethanchang13 <526ychang@frhsd.com>
2025-04-24 09:26:58 -0400

    add themed images

---
+++ script.js
@@ -0,0 +1,32 @@
+var names = [
+  "shell",
+  "star",
+  "bobomb",
+  "mario",
+  "luigi",
+  "peach",
+  "1up",
+  "mushroom",
+  "thwomp",
+  "bulletbill",
+  "coin",
+  "goomba",
+];
+
+var imgs = [
+  "imgs/blueshell.png",
+  "imgs/star.png",
+  "imgs/bobomb.png",
+  "imgs/mario.png",
+  "imgs/luigi.png",
+  "imgs/peach.png",
+  "imgs/1up.png",
+  "imgs/mushroom.png",
+  "imgs/thwomp.png",
+  "imgs/bulletbill.png",
+  "imgs/coin.png",
+  "imgs/goomba.png",
+];
+
+var namesFull = names.concat(names);
+var imgsFull = imgs.concat(imgs);
